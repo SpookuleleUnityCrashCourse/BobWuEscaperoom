690087921bb8dcbfa3b2008e6db49e1f57ad7852
wujingkai111 <67622114+wujingkai111@users.noreply.github.com>
2020-12-28 12:00:26 +0800

    Bobwu

---
+++ Assets/Scripts/Interaction/VoidBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidBox : MonoBehaviour, IInteractable
+{
+    public GameObject floor;
+
+    public void OnInteract()
+    {
+        Destroy(floor);
+    }
+
+    public string GetPrompt()
+    {
+        return "click to see the void";
+    }
+}
